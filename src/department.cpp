3efbddf98c49a00652aaa8aee4883684c0839c55
Shashank Agrawal <83277509+shashank-agwl@users.noreply.github.com>
2021-07-01 18:26:15 +0530

    Update department.cpp

--- src/department.cpp
+++ src/department.cpp
@@ -1,1 +1,17 @@
 #include "department.h"
+
+dept::set_name(string str){
+  this->name=str;
+}
+
+dept::set_ID(int user_id){
+  this->ID=user_id;
+}
+
+dept::get_ID(){
+  return ID;
+}
+
+dept::get_name(){
+  return name;
+}
