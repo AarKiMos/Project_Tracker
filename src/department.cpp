0ce8c9d8258d2e2b236947444aec5de63d462c0d
Shashank Agrawal <83277509+shashank-agwl@users.noreply.github.com>
2021-07-01 18:19:32 +0530

    Update department.cpp

--- src/department.cpp
+++ src/department.cpp
@@ -1,1 +1,18 @@
 #include "department.h"
+
+dept::set_name(string name){
+  this->name=name;
+}
+
+dept::set_ID(int ID){
+  this->ID=ID;
+}
+
+dept::get_ID(){
+  return ID;
+}
+
+dept::get_name(){
+  return name;
+}
+  
