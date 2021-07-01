656c407349bc3640794da29c9247ca77106ccc14
Shashank Agrawal <83277509+shashank-agwl@users.noreply.github.com>
2021-07-01 18:32:09 +0530

    Update project.cpp

--- src/project.cpp
+++ src/project.cpp
@@ -1,2 +1,25 @@
 #include "project.h"
 
+project::set_name(string str){
+  this->name=str;
+}
+
+project::set_ID(int user_id){
+  this->ID=user_id;
+}
+
+project::set_deadline( struct tm x){
+  this->deadline=x;
+}
+
+project::get_ID(){
+  return ID;
+}
+
+project::get_name(){
+  return name;
+}
+
+project::get_deadline(){
+  return deadline;
+}
