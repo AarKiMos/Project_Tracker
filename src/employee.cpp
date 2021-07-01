4926f73d1657cbe938ab79a69a465eb5e9f2ae67
Shashank Agrawal <83277509+shashank-agwl@users.noreply.github.com>
2021-07-01 18:28:46 +0530

    Update employee.cpp

--- src/employee.cpp
+++ src/employee.cpp
@@ -1,3 +1,17 @@
 #include "employee.h"
 
+emp::set_name(string str){
+  this->name=str;
+}
 
+emp::set_ID(int user_id){
+  this->ID=user_id;
+}
+
+emp::get_ID(){
+  return ID;
+}
+
+emp::get_name(){
+  return name;
+}
