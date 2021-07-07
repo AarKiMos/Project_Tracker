3d6136550ebb9f34b613c988c701f1f7487f34dc
Aachman Mittal <m.aachman@gmail.com>
2021-07-07 19:31:33 +0530

    Added MySQL integration wrapper db_util.h and db_util.cpp

---
+++ include/db_util.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * JIIT Noida 
+ * Data Structures group Mini Project
+ * Project: 
+ * Author: Aachman Mittal 9919103218
+******************************************************************************/
+
+#ifndef DB_UTIL_H
+#define DB_UTIL_H
+
+#include <iostream>
+#include <mysql/mysql.h>
+
+#define DB MYSQL
+#define DB_RES MYSQL_RES
+#define DB_ROW MYSQL_ROW
+
+struct connection_details
+{
+    const char *server,
+        *user,
+        *password,
+        *database;
+};
+
+MYSQL* db_connection_setup(struct connection_details);
+
+MYSQL_RES* db_perform_query(MYSQL*, const char*);
+
+void db_free_result(MYSQL_RES*);
+
+void db_close(MYSQL*);
+
+
+
+
+#endif
---
+++ src/db_util.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * JIIT Noida 
+ * Data Structures group Mini Project
+ * Project: 
+ * Author: Aachman Mittal 9919103218
+******************************************************************************/
+
+#include "db_util.h"
+
+MYSQL* db_connection_setup(struct connection_details db_details)
+{
+    MYSQL *connection = mysql_init(NULL); // mysql instance
+    
+    //connect database
+    if(!mysql_real_connect(connection, db_details.server, db_details.user, db_details.password, db_details.database, 0, NULL, 0)){
+        std::cout << "Connection Error: " << mysql_error(connection) << std::endl;
+        exit(1); 
+    }
+
+    return connection;
+}
+
+MYSQL_RES* db_perform_query(MYSQL *connection, const char *sql_query)
+{
+    //send query to db
+    if(mysql_query(connection, sql_query)){
+        std::cout << "MySQL Query Error: " << mysql_error(connection) << std::endl;
+        exit(1);
+    }
+
+    return mysql_use_result(connection);
+}
+
+void db_free_result(MYSQL_RES* res)
+{
+    mysql_free_result(res);
+}
+
+void db_close(MYSQL* conn)
+{
+    mysql_close(conn);
+}
