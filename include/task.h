82978c6c16957bd7e38be1bd909ee310578905da
Aachman Mittal <m.aachman@gmail.com>
2021-07-19 02:46:01 +0530

    Fixes: Major Bug Fixes

--- include/task.h
+++ include/task.h
@@ -25,6 +25,7 @@ private:
 public:
 
     int status; // 0- not ready, 1- ready but unassigned, 2- assigned 3-completed 
+    int dtd;
     task* prev;
     task* next;    // To create data structures
 
@@ -40,6 +41,15 @@ public:
     int get_PID();
     struct tm get_deadline();
 
+};
+
+class task_comparator
+{
+ public:
+   bool operator()(const task& a, const task& b)
+   {
+        return a.dtd < b.dtd;
+   }
 };
 
 
